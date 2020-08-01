a4c30dc58589877699662e6b3fceadb8c97dc7c0
SravaniDurga123 <sravanidurga.maragani@gmail.com>
2020-08-01 17:35:13 +0530

    second

--- truyumStoreWeb/Js/javascipt.js
+++ truyumStoreWeb/Js/javascipt.js
@@ -1,31 +1,44 @@
-
+var name1,price1,freedelivery1,category1;
 function addtocart(name,freedelivery,price,category){
     console.log(name);
     console.log("dasd");
+    var favoritemovie = "Shrek";
+sessionStorage.setItem("name",name);
+sessionStorage.setItem("freedelivery",freedelivery);
+sessionStorage.setItem("price",price);
+sessionStorage.setItem("category",category);
+}
 
-    var table=document.getElementById("mytable");
-    table.innerHTML='ffdv';
-    let row=document.createElement('tr');
-    let col1=document.createElement('td');
-    let col2=document.createElement('td');
-    let col3=document.createElement('td');
-    let col4=document.createElement('td');
-      col1.textContent=name;
-       row.appendChild(col1);
-       col2.textContent=freedelivery;
-       row.appendChild(col2);
-       col3.textContent=price;
-       row.appendChild(col3);
-       col4.textContent=category;
-       row.appendChild(col4);
-       document.querySelector("mytable").appendChild(row);
-    // var cell1=table.insertCell(0);
-    // var cell2=table.insertCell(1);
-    // var cell3=table.insertCell(2);
-    // var cell4=table.insertCell(3);
-    // cell1.innerHTML='name';
-    // cell2.innerHTML=freedelivery;
-    // cell3.innerHTML=price;
-    // cell4.innerHTML=category;
-   
+
+function addtocart1(){
+  var table=document.getElementById("mytable");
+   var array= {
+     name:sessionStorage.getItem("name"),
+     freedelivery:sessionStorage.getItem("freedelivery"),
+     category:sessionStorage.getItem("category"),
+     price:sessionStorage.getItem("price")
+   };
+   var row=table.insertRow(-1);
+   var cell1=row.insertCell(0);
+  var cell2=row.insertCell(1);
+  var cell3=row.insertCell(2);
+  var cell4=row.insertCell(3);
+  var cell5=row.insertCell(4);
+  var cell6=row.insertCell(5);
+   for (){
+      
+   }
+//   var row=table.insertRow(-1);
+//   var cell1=row.insertCell(0);
+//   var cell2=row.insertCell(1);
+//   var cell3=row.insertCell(2);
+//   var cell4=row.insertCell(3);
+//   var cell5=row.insertCell(4);
+//   var cell6=row.insertCell(5);
+//   cell1.innerHTML=sessionStorage.getItem("name");
+//   cell2.innerHTML=" ";
+//   cell3.innerHTML=sessionStorage.getItem("freedelivery");
+//   cell4.innerHTML=sessionStorage.getItem("price");
+//   cell5.innerHTML=sessionStorage.getItem("category");
+//  cell6.innerHTML="<a >Deletefromcart</a>";
 }
